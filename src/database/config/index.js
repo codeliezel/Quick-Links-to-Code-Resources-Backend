5103cf3dcdef86b6014cd6c16f30443713c40b4d
Funmilayo Olaiya <olufunmilayo335@gmail.com>
2020-03-30 20:51:55 +0100

    fx(codeCoverage): fix failing test coverage

--- src/database/config/index.js
+++ src/database/config/index.js
@@ -1,24 +1,30 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-require('dotenv').config();
+dotenv.config();
 
+const prodUri = process.env.PROD_DATABASE_URI;
 const devUri = process.env.DEV_DATABASE_URI;
-const testUri = `mongodb+srv://${process.env.name}:${process.env.password}@${process.env.database}/${process.env.dbtable}?retryWrites=true&w=majority`;
+const testUri = `mongodb+srv://${process.env.name}:${process.env.password}@${process.env.database}/${process.env.dbtable}`;
 
-mongoose.connect(devUri,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-  })
-  .then(() => {
-    console.log('Connected to database!');
-  })
-  .catch((error) => {
-    console.log('Connection failed!');
-    console.log(error);
-  });
+// connection for the dev environment
+if (process.env.NODE_ENV === 'development') {
+  mongoose.connect(devUri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log('Connected to database!');
+    })
+    .catch((error) => {
+      console.log('Connection failed!');
+      console.log(error);
+    });
+}
 
+// connection for the test environment
 if (process.env.NODE_ENV === 'test') {
   mongoose.connect(testUri,
     {
@@ -35,3 +41,19 @@ if (process.env.NODE_ENV === 'test') {
     });
 }
 
+// connection for the production environment
+if (process.env.NODE_ENV === 'production') {
+  mongoose.connect(prodUri,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      console.log('Connected to prod database!');
+    })
+    .catch((error) => {
+      console.log('Connection failed!');
+      console.log(error);
+    });
+}
